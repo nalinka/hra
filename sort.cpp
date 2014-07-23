6e0048cc7b4888d6d5c256f332664f5e1145fe91
nalinka <natalia.tokarova@gmail.com>
2014-07-23 17:07:29 +0200

    pridany sort.cpp

---
+++ sort.cpp
@@ -0,0 +1,12 @@
+sort{
+  for(int i; i < list.size; i++{
+    for(int j; j< list.size; j ++){
+      if(list[j] < list[j+1]){
+        conitnoue;
+      else{
+        list.swap;
+      }
+      }
+    }
+  }
+}
