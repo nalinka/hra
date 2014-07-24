8c2ab8a86faa028f7676cb525a29ad882720b4c0
nalinka <natalia.tokarova@gmail.com>
2014-07-24 14:54:08 +0200

    pridany insert sort

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 main: main.cpp ship.h ship.cpp world.h world.cpp ship_ai.h ship_ai.cpp 
 	g++ main.cpp world.cpp ship.cpp ship_ai.cpp -o main -O3 -std=c++0x
+
+busort: busort.cpp 
+	g++ busort.cpp -o busort
---
+++ busort.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+vector <int> bubble(vector<int> list);
+vector <int> insert(vector<int> list);
+
+int main(){
+  int pocet;
+  vector<int> list;
+  vector<int> zoznam_vrat;
+  scanf("%d", &pocet);
+  list.resize(pocet);
+  for(int i = 0 ; i < pocet; i++) {
+    scanf("%d", &list[i]);
+  }
+  
+  zoznam_vrat = bubble(list);
+  //zoznam_vrat = insert(list);
+  
+  for(int i = 0 ; i < list.size(); i++) {
+    printf("%d \n", zoznam_vrat[i]);
+  }
+}
+
+vector <int> bubble(vector<int> list) {
+  
+  for(int i = 0; i < list.size(); i++) {
+    int swaped = 0;
+    for(int j = 0; j + 1 < list.size() - i; j++) {
+      if(list[j] > list[j + 1]) {
+        swap(list[j], list[j + 1]);
+        swaped = swaped + 1;
+      }
+    }
+    if (swaped == 0)
+      break; 
+  }
+  
+  return list; 
+}
+
+vector <int> insert(vector<int> list) {
+  
+  for(int i = 1; i < list.size(); i++) {
+    int swaped = 0;
+    for(int j = 0; j < list.size() ; j++) {
+      if(list[j] > list[i]) {
+          swap(list[i], list[j]);
+          swaped = swaped + 1;
+       }
+     }
+ 
+    if (swaped == 0)
+      break; 
+  }
+  return list; 
+}
+
